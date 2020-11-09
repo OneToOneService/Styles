3c96780a63893c5e9f67997b4c1ab12c357c4e2e
JC <jashwant.chouhan@gmail.com>
2020-11-09 19:18:01 +0530

    updated for vue build

---
+++ js/about.1b337f2c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"4b1a":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h2",[t._v("Rich Text Editor")])])}],i={name:"Richtext"},r=i,s=n("2877"),u=Object(s["a"])(r,a,c,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.1b337f2c.js.map
