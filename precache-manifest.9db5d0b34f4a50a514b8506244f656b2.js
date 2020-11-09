942fb6cc5b0435f8da6c285f708659bea6150a50
JC <jashwant.chouhan@gmail.com>
2020-11-09 19:57:47 +0530

    Revert "updated for vue build"
    This reverts commit 3c96780a63893c5e9f67997b4c1ab12c357c4e2e.

---
+++ precache-manifest.9db5d0b34f4a50a514b8506244f656b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24e28c4bdc03350a4160a71b08b7743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7289697b4f662851574c",
+    "url": "/static/css/main.523ee4e1.chunk.css"
+  },
+  {
+    "revision": "517e397d5c5942cb74c5",
+    "url": "/static/js/2.438e7210.chunk.js"
+  },
+  {
+    "revision": "1e7a7346baa31f7b98172df30d46cdba",
+    "url": "/static/js/2.438e7210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7289697b4f662851574c",
+    "url": "/static/js/main.2d0f8cd4.chunk.js"
+  },
+  {
+    "revision": "f730f3477dc2779ee533",
+    "url": "/static/js/runtime-main.a140644f.js"
+  },
+  {
+    "revision": "e86c62802179e40f47b1e940c06944f0",
+    "url": "/static/media/logo.e86c6280.png"
+  }
+]);
